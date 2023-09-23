9fd70cccc7795444e47bcaea0b69b10354e70ba9
Andy McCoy <andy.k.mccoy@gmail.com>
2023-09-23 15:02:14 -0700

    add cloudflare client

---
+++ src/cloudflareClient.js
@@ -0,0 +1,33 @@
+import p from 'phin';
+
+export class CloudflareClient {
+  #bearerToken;
+  #client;
+  #defaultOptions = {
+    parse: 'json',
+    hostname: 'https://api.cloudflare.com',
+    core: {
+      headers: {
+        'Authorization': `Bearer ${this.#bearerToken}`,
+        'Content-Type': 'application/json'
+      }
+    }
+  };
+
+  constructor(bearerToken) {
+    this.#bearerToken = bearerToken;
+    this.#client = p.defaults(this.#defaultOptions);
+  }
+
+  async request(options) {
+    const { body } = await this.#client(options);
+
+    if (body.success) {
+      return body.result;
+    }
+
+    const errorString = body.errors.reduce((accum, error) => accum + `${error.code}:${error.message}\n`, '');
+
+    throw new Error(errorString);
+  }
+}
---
+++ src/cloudflareClient.test.js
@@ -0,0 +1,81 @@
+import { describe, beforeEach, afterEach, test, expect, vi, afterAll } from 'vitest';
+import { CloudflareClient } from './cloudflareClient';
+import p from 'phin';
+
+vi.mock('phin');
+
+describe('CloudflareClient successful request', () => {
+  const mockToken = 'mock-bearer-token';
+  let reqSpy, clientSpy, resp;
+
+  beforeEach(async () => {
+    reqSpy = vi.fn(async () => {
+      return {
+        body: {
+          success: true,
+          result: ['result']
+        }
+      }
+    });
+    clientSpy = vi.fn(reqSpy);
+    p.defaults.mockReturnValue(clientSpy);
+
+    resp = await (new CloudflareClient(mockToken)).request({
+      method: 'GET',
+      path: '/api/test/path'
+    });
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  afterAll(() => {
+    vi.resetAllMocks();
+  });
+
+  test('should make a GET request to the test path', () => {
+    expect(reqSpy).toBeCalledWith({
+      method: 'GET',
+      path: '/api/test/path'
+    });
+  });
+
+  test('should return the the parsed JSON body', () => {
+    expect(resp).toStrictEqual(['result']);
+  });
+});
+
+describe('CloudflareClient failed request', () => {
+  const mockToken = 'mock-bearer-token';
+  let reqSpy, clientSpy, client;
+
+  beforeEach(async () => {
+    reqSpy = vi.fn(async () => {
+      return {
+        body: {
+          success: false,
+          errors: [{code: 404, message: 'not found'}, {code: 401, message: 'not authorized'}]
+        }
+      }
+    });
+    clientSpy = vi.fn(reqSpy);
+    p.defaults.mockReturnValue(clientSpy);
+    client = new CloudflareClient(mockToken);
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  afterAll(() => {
+    vi.resetAllMocks();
+  });
+
+  test('should reject with concatinated errors', async () => {
+    await expect(client.request({
+      method: 'GET',
+      path: '/api/test/path'
+    })).rejects.toThrow('404:not found\n401:not authorized\n');
+  });
+});
